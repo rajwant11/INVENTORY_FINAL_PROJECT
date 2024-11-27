dbfdd9df37ebd96b17a1f7d42101a47f963f8688
Rajwant <deo.rajwant13@gmail.com>
2024-11-27 12:13:15 -0500

    TESTING COMMIT

--- src/FreshProduct.java
+++ src/FreshProduct.java
@@ -3,7 +3,7 @@ public class FreshProduct extends Product {
     public FreshProduct(String name, int quantity, double price) {
         super(name, quantity, price);
     }
-
+// TESTING GIT
     @Override
     public void displayProductInfo() {
         System.out.println("Fresh Product - Name: " + name + ", Quantity: " + quantity + ", Price: $" + price);
